dee59123ef4d20eb8cbd08ad13bcf4bfaa304dfb
Jake Zook <jakezook01@gmail.com>
2023-09-21 15:03:29 -0600

    Fixed logic to pass input to constructor functions. Added ability to then pass the data into a writeFile function to generate the logo.svg.

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const getInput = require('./lib/input');
+const createObject = require('./lib/shapes');
 
-function init () {
-    getInput();
+const fs = require('fs');
+
+async function init () {
+    const data = await getInput();
+    const shapeData = await createObject(data);
+    generateFile(shapeData);
+}
+
+function generateFile(shapeData) {
+    const obj = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+    ${shapeData}
+
+    </svg>
+    `;
+
+
+    fs.writeFile('logo.svg', obj, (err) =>
+    err ? console.error(err) : console.log('Success!')
+    );
 }
 
 init();
